8975c84d3af07b4fba20fe4e8d882f1f75fb4a50
barsbold <94489818ok@gmail.com>
2025-10-29 10:26:48 +0800

    shuk

--- express-server/BackEnd/controller/foodCategory.controller.js
+++ express-server/BackEnd/controller/foodCategory.controller.js
@@ -3,10 +3,18 @@ import { FoodCategory } from "../models/user.model"
 export const createFoodCategory = async (req, res)=>{
   try {
     const { body } = req;
-
     const foodCategory = await FoodCategory.create(body);
     res.status(200).send({message:"Amjilttai nemlee", data: foodCategory});
   } catch (error) {
     res.statis(500).send({message:"Aldaa garlaa", data: null});
   }
+}
+
+export const updateFood = async (req, res) => {
+try {
+  const {id} = req.body
+  
+} catch (error) {
+  
+}
 }
